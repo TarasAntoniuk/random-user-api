29ad7bfefc7010492273fc3348b850b6dc956b18
taras <bronya2004@gmail.com>
2026-03-02 17:25:07 +0100

    fix: deleted unused import

--- src/main/java/com/tarasantoniuk/random_user_api/feature/user/exception/ExternalApiException.java
+++ src/main/java/com/tarasantoniuk/random_user_api/feature/user/exception/ExternalApiException.java
@@ -1,7 +1,5 @@
 package com.tarasantoniuk.random_user_api.feature.user.exception;
 
-import java.time.LocalDateTime;
-
 public class ExternalApiException extends RuntimeException {
     private final int statusCode;
 
