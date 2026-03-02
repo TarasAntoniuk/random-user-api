b9314f4d8aebcfa015efac7c5b1c2f2bca4fe68a
taras <bronya2004@gmail.com>
2026-03-02 21:06:28 +0100

    feat: add configurable connection and read timeouts for external API client

--- src/main/java/com/tarasantoniuk/random_user_api/feature/user/client/RandomUserClient.java
+++ src/main/java/com/tarasantoniuk/random_user_api/feature/user/client/RandomUserClient.java
@@ -2,11 +2,17 @@ package com.tarasantoniuk.random_user_api.feature.user.client;
 
 import com.tarasantoniuk.random_user_api.feature.user.dto.UserResponseDto;
 import com.tarasantoniuk.random_user_api.feature.user.exception.ExternalApiException;
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 
 @Component
@@ -16,14 +22,21 @@ public class RandomUserClient {
 
     private final static Logger log = LoggerFactory.getLogger(RandomUserClient.class);
 
-    @Value("${randomuser.base-url}")
-    private String baseUrl;
-
     public RandomUserClient(RestClient.Builder builder,
-                            @Value("${randomuser.base-url}") String baseUrl) {
+                            @Value("${randomuser.base-url}") String baseUrl,
+
+                            @Value("${randomuser.connect-timeout}") int connectTimeout,
+                            @Value("${randomuser.read-timeout}") int readTimeout) {
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(connectTimeout));
+        requestFactory.setReadTimeout(Duration.ofSeconds(readTimeout));
+
         this.restClient = builder
                 .baseUrl(baseUrl)
+                .requestFactory(requestFactory)
                 .build();
+
     }
 
     public UserResponseDto getUsers(int count) {
